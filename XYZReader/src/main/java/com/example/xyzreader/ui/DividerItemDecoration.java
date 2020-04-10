5e02a44d8b64343ed5c0c16bb9261a5ee0053772
Joel Oguntoye <joeloguntoye@gmail.com>
2020-04-10 16:45:54 +0100

    Fixing starter code gradle issues

--- XYZReader/src/main/java/com/example/xyzreader/ui/DividerItemDecoration.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DividerItemDecoration.java
@@ -19,9 +19,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+
 
 public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
